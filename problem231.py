f6422f7e41ee672d9e8074af6f5e57dbf78417ae
talha atif <talhaatif@gmail.com>
2024-10-14 23:25:06 +0500

    Auto commit: Mon Oct 14 23:25:06 2024

--- problem231.py
+++ problem231.py
@@ -3,7 +3,8 @@ def solve():
     for _ in range(int(input())):
         x,y,z = map(int,input().split())
         temp = 0
-        if (x==1 or y==1) or (x==1 or z==1) or (y==1 or z==1)
+        if (x==1 and y==1) or (x==1 or z==1) or (y==1 or z==1):
+
         
     
     
