e030637db6aeabd38859e8bd3566da14b2ba8893
talha atif <talhaatif@gmail.com>
2024-10-14 23:25:36 +0500

    Auto commit: Mon Oct 14 23:25:36 2024

--- problem231.py
+++ problem231.py
@@ -2,10 +2,10 @@ def solve():
     count = 0
     for _ in range(int(input())):
         x,y,z = map(int,input().split())
-        temp = 0
         if (x==1 and y==1) or (x==1 and z==1) or (y==1 and z==1):
            count+=1
         
+    print(count)
 
         
     
