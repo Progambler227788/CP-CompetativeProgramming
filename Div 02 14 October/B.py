df20a57b173662ed3e8e4e5bf3a70378cec96272
talha atif <talhaatif@gmail.com>
2024-10-15 21:56:18 +0500

    One problem upsolved

--- Div 02 14 October/B.py
+++ Div 02 14 October/B.py
@@ -7,3 +7,35 @@
 
 # if x is 1:
 #    sum of array
+
+# suming then dividing by x
+import math
+def solve(n,x,array):
+    # rem = 0
+    # if x>n:
+    #     x = n    
+    # data = sorted(array, reverse= True)
+    # done = 0
+    print(   max(  max(array) , int(math.ceil( sum(array) / x ))))
+
+for _ in range(int(input())):
+    n,x = map(int,input().split())
+    arr = list(map(int,input().split()))
+    solve(n,x,arr)
+
+
+
+''' 
+3 2
+3 1 2
+1
+3 2 1
+
+1 1
+
+2 1 1
+
+1 1
+
+1 0 1
+'''
